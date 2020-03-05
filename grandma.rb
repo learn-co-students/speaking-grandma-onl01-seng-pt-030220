5f379e6185f29e03d01fa3fd21cd45cdee06a236
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-05 15:15:39 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.downcase == phrase
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase = "Hi!"
-    return "HUH?! SPEAK UP, SONNY"
   end
 end
