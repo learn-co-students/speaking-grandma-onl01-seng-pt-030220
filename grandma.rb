5141162097bdc98e346d46c50ccb1fb0c1d9694a
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-07 22:54:13 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -17,11 +17,11 @@
 def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.upcase
-    puts "NO, NO SINCE 1938!"
+      return "NO, NO SINCE 1938!"
     else
-    puts "HUH?! SPEAK UP SONNY"
+      return "HUH?! SPEAK UP SONNY"
   end
 end
 
